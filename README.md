36f30e09207beb26a8f1f7d6b3d2018cbeca0184
krzynio <krzynio@serwer>
2016-10-31 11:56:03 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# python-samsung-smarttv-2016
-Samsung Smart TV 2016 remote in python
+# Samsung Smart TV 2016 remote in Python 3
+
+Samsung Smart TV 2016 remote in Python 3
+
+Usage from command line:
+
+Switch to channel 107
+```
+$ python sstv2016.py KEY_1 KEY_0 KEY_7 KEY_ENTER
+```
+
+In Python:
+```python
+from sstv2016 import remote
+remote('192.168.1.1', ['KEY_1','KEY_0','KEY_7','KEY_ENTER'])
+```
+
+Keys list:
+
+http://www.maartenvisscher.nl/samsung-tv-control/javadoc/nl/maartenvisscher/samsungtvcontrol/Keycode.html
+
---
+++ sstv2016.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import json
+import time
+import sys 
+
+def remote(ip_addr, keys_list):
+    asyncio.get_event_loop().run_until_complete(_remote(keys_list, ip_addr))
+
+@asyncio.coroutine
+def _remote(keys, tv_addr,delay=1):
+    websocket = yield from websockets.connect('ws://%s:%d/api/v2/channels/samsung.remote.control' % (tv_addr,8001))
+    if type(keys) is str:
+        _keys = [keys]
+    else:
+        _keys = keys
+
+    try:
+        while True:
+            message = yield from websocket.recv()
+            parsed = json.loads(message)
+            if (parsed['event'] == 'ms.channel.connect'):  
+                k = 0
+                for key in _keys:
+                    k = k + 1
+                    cmd = '{"method":"ms.remote.control","params":{"Cmd":"Click","DataOfCmd":"%s","Option":"false","TypeOfRemote":"SendRemoteKey"}}' % key
+                    yield from websocket.send(cmd)
+                    if k != len(_keys):
+                        time.sleep(delay)                        
+                break
+
+    finally:
+        yield from websocket.close()
+
+if __name__ == "__main__":
+    remote(sys.argv[1], sys.argv[2:])
